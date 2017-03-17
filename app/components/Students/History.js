79429b09376601d65f48afe02d02d08aeaaf0a3d
ameno- <ameno.osman13@gmail.com>
2017-03-16 19:00:58 -0700

    working on history

--- app/components/Students/History.js
+++ app/components/Students/History.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-import { 
-    StyleSheet, 
-    View, 
-    Text,
-    TouchableOpacity,
-    ScrollView
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, ScrollView} from 'react-native';
 
-import { Col, Row, Grid } from "react-native-easy-grid";
+import {Col, Row, Grid} from "react-native-easy-grid";
 
-import { getStudentHistoryAsync } from '../../db';
+import {getStudentHistoryAsync} from '../../db';
 
 class HistoryList extends Component {
     constructor() {
@@ -20,60 +14,134 @@ class HistoryList extends Component {
         }
     }
     componentDidMount() {
-        getStudentHistoryAsync(this.props.student.id).then(result => this.setState({
-            historyData: JSON.parse(result)
-        }))
+        getStudentHistoryAsync(this.props.student.id).then(result => {
+            console.log(result);
+            this.setState({
+                historyData: JSON.parse(result)
+            })
+        });
+    }
+
+    buildGridGroups() {
+        var train = [];
+        var airplane = [];
+        var mouse = [];
+        var baby = [];
+        var snake = [];
+        var train = [];
+        var pizza = [];
+
+        this.state.historyData.forEach(function (item) {
+            switch (item.correctAnswer) {
+                case "train":
+                    train.push(item);
+                    break;
+                case "airplane":
+                    airplane.push(item);
+                    break;
+                case "train":
+                    train.push(item);
+                    break;
+                case "baby":
+                    baby.push(item);
+                    break;
+                case "mouse":
+                    mouse.push(item);
+                    break;
+                case "pizza":
+                    pizza.push(item);
+                    break;
+                case "snake":
+                    snake.push(item);
+                    break;
+            }
+        });
     }
 
     render() {
         const {student} = this.state;
         const renderHistory = () => {
             if (this.state.historyData != 'Nothing found') {
-                return this.state.historyData.map((item, i) => {
-                    return (
-                            <Grid key={i}>
+                return this
+                    .state
+                    .historyData
+                    .map((item, i) => {
+                        return (
+                            <Grid key={i} style={styles.mainGrid}>
                                 <Row style={styles.topRow}>
                                     <Col></Col>
-                                    <Col><Text>ling 1</Text></Col>
-                                    <Col><Text>ling 2</Text></Col>
-                                    <Col><Text>ling 3</Text></Col>
-                                    <Col><Text>ling 4</Text></Col>
-                                    <Col><Text>ling 5</Text></Col>
-                                    <Col><Text>ling 6</Text></Col>
+                                    <Col>
+                                        <Text>ling 1</Text>
+                                    </Col>
+                                    <Col>
+                                        <Text>ling 2</Text>
+                                    </Col>
+                                    <Col>
+                                        <Text>ling 3</Text>
+                                    </Col>
+                                    <Col>
+                                        <Text>ling 4</Text>
+                                    </Col>
+                                    <Col>
+                                        <Text>ling 5</Text>
+                                    </Col>
+                                    <Col>
+                                        <Text>ling 6</Text>
+                                    </Col>
                                 </Row>
                                 <Col style={styles.leftCol}>
                                     <Row></Row>
                                     <Row>
                                         <Col>
-                                            <Text>ling 1</Text>
+                                            <Text>Ling sound: "ah"</Text>
                                         </Col>
-                                        <Col><Text>Da ansa</Text></Col>
-                                        <Col><Text>Da ansa2</Text></Col>
-                                        <Col><Text>Da ansa3</Text></Col>
-                                        <Col><Text>Da ansa4</Text></Col>
-                                        <Col><Text>Da ansa5</Text></Col>
-                                        <Col><Text>Da ansa6</Text></Col>
+                                        <Col>
+                                            <Text>Da ansa</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>Da ansa2</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>Da ansa3</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>Da ansa4</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>Da ansa5</Text>
+                                        </Col>
+                                        <Col>
+                                            <Text>Da ansa6</Text>
+                                        </Col>
                                     </Row>
-                                    <Row><Text>ling 2</Text></Row>
-                                    <Row><Text>ling 3</Text></Row>
-                                    <Row><Text>ling 4</Text></Row>
-                                    <Row><Text>ling 5</Text></Row>
-                                    <Row><Text>ling 6</Text></Row>
+                                    <Row>
+                                        <Text>ling 2</Text>
+                                    </Row>
+                                    <Row>
+                                        <Text>ling 3</Text>
+                                    </Row>
+                                    <Row>
+                                        <Text>ling 4</Text>
+                                    </Row>
+                                    <Row>
+                                        <Text>ling 5</Text>
+                                    </Row>
+                                    <Row>
+                                        <Text>ling 6</Text>
+                                    </Row>
                                 </Col>
                             </Grid>
 
-                        // <Text key={i}>
-                        //     Student name: {student.studentName}~{"\n"}
-                        //     Displayed sounds: {item.displayedSounds} {"\n"}
-                        //     Selected answer: {item.selectedAnswer} {"\n"}
-                        //     Correct answer: {item.correctAnswer}{"\n"}{"\n"}{"\n"}
-                        // </Text>
-                    );
-                });
+                        // <Text key={i}>     Student name: {student.studentName}~{"\n"}     Displayed
+                        // sounds: {item.displayedSounds} {"\n"}     Selected answer:
+                        // {item.selectedAnswer} {"\n"}     Correct answer:
+                        // {item.correctAnswer}{"\n"}{"\n"}{"\n"} </Text>
+                        );
+                    });
             }
         }
         return (
-             <ScrollView automaticallyAdjustContentInsets={false}>
+            <ScrollView automaticallyAdjustContentInsets={false}>
                 {renderHistory()}
             </ScrollView>
         );
@@ -81,11 +149,15 @@ class HistoryList extends Component {
 }
 
 const styles = StyleSheet.create({
+    mainGrid: {
+        marginTop: 100
+    },
     topRow: {
-        backgroundColor: 'red'
+        backgroundColor: '#D0D5E5',
+        padding: 10
     },
     leftCol: {
-        backgroundColor: 'blue'
+        backgroundColor: 'white'
     }
 })
 
