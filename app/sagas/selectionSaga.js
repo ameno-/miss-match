3c6d6dfb8d0ca14562341dbef673cf7c51789636
ameno- <ameno.osman13@gmail.com>
2017-03-19 17:37:26 -0700

    cleanup

--- app/sagas/selectionSaga.js
+++ app/sagas/selectionSaga.js
@@ -1,11 +1,11 @@
-import {call, put, takeEvery, takeLatest} from 'redux-saga/effects';
+import {call, put, takeEvery} from 'redux-saga/effects';
 import {getRandomCollection} from '../../utils/helper';
 import {updateStudentDataAsync} from '../db';
 
 function * selectionChanged(action) {
   try {
-    console.log(action.lingSoundCount)
     if (action.selectedSoundIndex === action.correctSoundIndex) {
+
       yield put({
         type: "QUESTIONS_NEW",
         payload: getRandomCollection(action.testIndex, action.lingSoundCount)
@@ -13,7 +13,7 @@ function * selectionChanged(action) {
     } else {
       yield put({type: "QUESTIONS_REDO"})
     }
-    console.log(action);
+
     yield updateStudentDataAsync(action.currentStudent, action.visualProp, action.displayedSounds, action.selectedSoundIndex, action.correctSoundIndex);
   } catch (e) {
     yield console.log("done fucked up");
